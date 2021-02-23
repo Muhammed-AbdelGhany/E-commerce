a6c9a03a3c4c3a5ab6c89548441504f92d606e3a
unknown <muhammedabdelghany5297@gmail.com>
2021-02-23 18:51:29 +0200

    Starting networking.

--- lib/widgets/cart_product.dart
+++ lib/widgets/cart_product.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart';
+import 'badge.dart';
 
 class CartProduct extends StatelessWidget {
   final String id;
